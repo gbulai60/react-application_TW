40d479a500168ccdd14cecf4a25ec48f9b097952
= <=>
2024-03-07 12:09:29 +0200

    Adding styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import PersonLayout from "./PersonLayout";
+import "./App.css";
 
 const App = () => {
-  const [showInfo, setShowInfo] = useState(false);
-  const handleButtonClick = () => {
-    setShowInfo(!showInfo);
+    const [showInfo, setShowInfo] = useState(false);
+    const [buttonText, setButtonText] = useState("View Data");
+    const handleButtonClick = () => {
+        setShowInfo(!showInfo);
+        setButtonText(showInfo ? "View Data" : "Hide Data");
+    };
 
-  };
-
-  return (
-      <div style={{ textAlign: "center"}}>
-        <button onClick={handleButtonClick}>View Data</button>
-        {showInfo && <PersonLayout />}
-      </div>
-  );
+    return (
+        <div className="container">
+            <button className="button" onClick={handleButtonClick}>
+                {buttonText}
+            </button>
+            {showInfo && <div className="spacer" />}
+            {showInfo && <PersonLayout />}
+        </div>
+    );
 };
 
 export default App;
